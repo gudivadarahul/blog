79c551159767ce19f74cca9c40e4c1e64d2ede12
gudivadarahul <gudivadarahul@gmail.com>
2022-05-09 08:53:45 -0500

    added more components

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,21 @@
+import { useState } from "react"
+
 const Home = () => {
+
+    const [blogs, setBlogs] = useState([
+        {title: "Blog1", body: "asdfjkl;", author: 'auth1', id: 1},
+        {title: "Blog12", body: "asdfjkl;", author: 'auth12', id: 2},
+        {title: "Blog13", body: "asdfjkl;", author: 'auth13', id: 3}
+    ])
+
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                </div>
+            ))}
         </div>
      );
 }
